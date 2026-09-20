11688d124e0e32bae950a30702784479987cedd5
ExcellenceOseagwina <oseagwinaexcellence@gmail.com>
2026-09-20 13:32:29 +0100

    c

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ const passWord = document.querySelector("#password");
 const errorPassword = document.querySelector(".errorPassword");
 const passwordNameLength = document.querySelector(".passwordNameLength");
 const loginButton = document.querySelector(".submit");
-const loginSuccessful = document.querySelector('.loginsuccessful');
+const loginSuccessful = document.querySelector(".loginMessage");
+
 function showError(event) {
-    event.preventDefault();
-    let isValid = true;
+  event.preventDefault();
+  let isValid = true;
   //Username
   if (userName.value.trim() === "") {
     errorUsername.style.display = "block";
@@ -36,24 +37,45 @@ function showError(event) {
     errorPassword.style.display = "none";
     passwordNameLength.style.display = "none";
   }
-  loginSuccessful.classList.add("show");
-
 
   if (isValid) {
-
     loginSuccessful.classList.add("show");
-
     setTimeout(function () {
-        loginSuccessful.classList.remove("show");
+      loginSuccessful.classList.remove("show");
     }, 5000);
-
-} else {
-
+  } else {
     loginSuccessful.classList.remove("show");
 
     console.log("Please fix the errors");
-}
+  }
 }
 
+function typingUsername() {
+  if (userName.value.trim() === "") {
+    errorUsername.style.display = "block";
+    userNameLength.style.display = "none";
+  } else if (userName.value.trim().length < 6) {
+    userNameLength.style.display = "block";
+    errorUsername.style.display = "none";
+  } else {
+    userNameLength.style.display = "none";
+    errorUsername.style.display = "none";
+  }
+}
+
+function typingPassword() {
+  if (passWord.value.trim() === "") {
+    errorPassword.style.display = "block";
+    passwordNameLength.style.display = "none";
+  } else if (passWord.value.trim().length < 6) {
+    passwordNameLength.style.display = "block";
+    errorPassword.style.display = "none";
+  } else {
+    errorPassword.style.display = "none";
+    passwordNameLength.style.display = "none";
+  }
+}
 //Login Button To Perform An Action
-loginButton.addEventListener("click", showError);
+loginButton.addEventListener("submit", showError);
+userName.addEventListener('input', typingUsername);
+passWord.addEventListener('input', typingPassword);
